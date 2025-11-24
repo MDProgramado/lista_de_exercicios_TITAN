1e19eae0cefc81526bb53c7667bc3d88c8006ea5
Maicon Douglas <mdprogamacao@outlook.com>
2025-11-24 14:28:07 -0300

    RESOLVENDO OS EXERCICIOS DE 1 ATÉ 6

---
+++ exer01.js
@@ -0,0 +1,20 @@
+let numero1 = 10 
+let numero2 = 20
+
+function soma(numero1, numero2) {
+
+    let resultadoSoma = numero1 + numero2;
+    let resultadoSubstracao = numero1 - numero2;
+    let resultadoMultiplicacao = numero1 * numero2;
+    let resultadoDivisao = numero1 / numero2;
+
+    const resultados =  "O resultado da soma é " 
+    + resultadoSoma +
+    "\n, o resultado da subtração é " 
+    + resultadoSubstracao +
+     "\n, o resultado da duvisãao é "
+     + resultadoDivisao +
+     "\n, o resultado da divisão é"
+      + resultadoDivisao 
+    return  resultados
+} 
